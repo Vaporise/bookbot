68e12ea6beaede505c2b672a7418db16fadf0b8e
Vaporise <draxdragonfire@googlemail.com>
2024-09-30 23:43:54 +0900

    Added code to sort the output in alphabetical order.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def main():
         file_contents = f.read()
         word_count = count_words(file_contents)
         print(f"Word count: {word_count}")
+
         num_count = num_of_times(file_contents)
+        sorted_count = sorting(num_count)
+
         print("Number of times each letter appears:")
-        for char, count in num_count.items():
+        for char, count in sorted_count:
              print (f"'{char}': {count}")
+        
 
 def count_words(text):
     words = text.split()
@@ -17,12 +21,18 @@ def num_of_times(count):
     lower_text = count.lower()
     counting = {}
     for letter in lower_text:
-        if letter in counting:
-                counting[letter] += 1
-        else:
-                counting[letter] = 1
+            if letter.isalpha():
+                if letter in counting:
+                    counting[letter] += 1
+                else:
+                    counting[letter] = 1
     return counting
 
+def sorting(counting):
+     counting_tuples = list(counting.items())
+     counting_tuples.sort()
+     return counting_tuples
+     
             
 
 main()
