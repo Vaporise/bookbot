2bcf3e87621f10ccc5d29e09c9d7ee6257c6115f
Vaporise <draxdragonfire@googlemail.com>
2024-09-30 22:50:19 +0900

    Added more code to count the words in the txt file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+def count_words(file_contents):
+    words = file_contents.split()
+    word in len(words)
+    word_count = word + 1
+
+    print(word)
+    return word
+    
+
+main()
+
+
