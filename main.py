3cba4ab87d29e318b8a8641f0a922782c25b97bf
Vaporise <draxdragonfire@googlemail.com>
2024-09-30 23:20:17 +0900

    Added all the code to list the number of times each letter appears in the text file.

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        word_count = count_words(file_contents)
+        print(f"Word count: {word_count}")
+        num_count = num_of_times(file_contents)
+        print("Number of times each letter appears:")
+        for char, count in num_count.items():
+             print (f"'{char}': {count}")
 
-def count_words(file_contents):
-    words = file_contents.split()
-    word in len(words)
-    word_count = word + 1
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
 
-    print(word)
-    return word
-    
+def num_of_times(count):
+    lower_text = count.lower()
+    counting = {}
+    for letter in lower_text:
+        if letter in counting:
+                counting[letter] += 1
+        else:
+                counting[letter] = 1
+    return counting
+
+            
 
 main()
 
